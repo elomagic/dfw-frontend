ed421fbc7a7c8c947bce2fa14c2d21b7fd2f8772
C. Rambow <rambow.dev@elomagic.de>
2024-10-22 20:42:37 +0200

    Sign in

--- src/pages/signin/SignInView.tsx
+++ src/pages/signin/SignInView.tsx
@@ -1,12 +1,208 @@
-import {Box, Card} from "@mui/material";
+import {
+    Box,
+    Button,
+    CssBaseline,
+    FormControl,
+    FormLabel,
+    Stack,
+    styled,
+    TextField,
+    Typography
+} from "@mui/material";
+import MuiCard from '@mui/material/Card';
+import Link from "@mui/material/Link";
+import ForgotPassword from "./ForgotPassword.tsx";
+import {useState} from "react";
 
-export default function SignInView() {
+const Card = styled(MuiCard)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignSelf: 'center',
+    width: '100%',
+    padding: theme.spacing(4),
+    gap: theme.spacing(2),
+    margin: 'auto',
+    [theme.breakpoints.up('sm')]: {
+        maxWidth: '450px',
+    },
+    boxShadow:
+        'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+    ...theme.applyStyles('dark', {
+        boxShadow:
+            'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+    }),
+}));
+
+const SignInContainer = styled(Stack)(({ theme }) => ({
+    height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
+    minHeight: '100%',
+    padding: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+        padding: theme.spacing(4),
+    },
+    '&::before': {
+        content: '""',
+        display: 'block',
+        position: 'absolute',
+        zIndex: -1,
+        inset: 0,
+        backgroundImage:
+            'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+        backgroundRepeat: 'no-repeat',
+        ...theme.applyStyles('dark', {
+            backgroundImage:
+                'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+        }),
+    },
+}));
+
+export default function SignInView(props: { disableCustomTheme?: boolean }) {
+
+    const [emailError, setEmailError] = useState(false);
+    const [emailErrorMessage, setEmailErrorMessage] = useState('');
+    const [passwordError, setPasswordError] = useState(false);
+    const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        if (emailError || passwordError) {
+            event.preventDefault();
+            return;
+        }
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    const validateInputs = () => {
+        const email = document.getElementById('email') as HTMLInputElement;
+        const password = document.getElementById('password') as HTMLInputElement;
+
+        let isValid = true;
+
+        if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
+            setEmailError(true);
+            setEmailErrorMessage('Please enter a valid email address.');
+            isValid = false;
+        } else {
+            setEmailError(false);
+            setEmailErrorMessage('');
+        }
+
+        if (!password.value || password.value.length < 6) {
+            setPasswordError(true);
+            setPasswordErrorMessage('Password must be at least 6 characters long.');
+            isValid = false;
+        } else {
+            setPasswordError(false);
+            setPasswordErrorMessage('');
+        }
+
+        return isValid;
+    };
 
     return (
-        <Box sx={{direction: 'row', display: 'flex', width: '100vh',}}>
-            <Card sx={{display: 'flex', width: '500px',}}>
-                SignInView
+        <SignInContainer direction="column" justifyContent="space-between">
+            <CssBaseline enableColorScheme />
+            <Card variant="outlined">
+
+                <Typography
+                    component="h2"
+                    variant="h4"
+                    sx={{ width: '100%', fontSize: 'unset' }}
+                >
+                    Dependency Firewall
+                </Typography>
+
+                <Typography
+                    component="h1"
+                    variant="h4"
+                    sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+                >
+                    Sign in
+                </Typography>
+
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit}
+                    noValidate
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '100%',
+                        gap: 2,
+                    }}
+                >
+                    <FormControl>
+                        <FormLabel htmlFor="email">Email</FormLabel>
+                        <TextField
+                            error={emailError}
+                            helperText={emailErrorMessage}
+                            id="email"
+                            type="email"
+                            name="email"
+                            placeholder="your@email.com"
+                            autoComplete="email"
+                            autoFocus
+                            required
+                            fullWidth
+                            variant="outlined"
+                            color={emailError ? 'error' : 'primary'}
+                            sx={{ ariaLabel: 'email' }}
+                        />
+                    </FormControl>
+
+                    <FormControl>
+                        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <FormLabel htmlFor="password">Password</FormLabel>
+                            <Link
+                                component="button"
+                                type="button"
+                                onClick={handleClickOpen}
+                                variant="body2"
+                                sx={{ alignSelf: 'baseline' }}
+                            >
+                                Forgot your password?
+                            </Link>
+                        </Box>
+                        <TextField
+                            error={passwordError}
+                            helperText={passwordErrorMessage}
+                            name="password"
+                            placeholder="••••••"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            autoFocus
+                            required
+                            fullWidth
+                            variant="outlined"
+                            color={passwordError ? 'error' : 'primary'}
+                        />
+                    </FormControl>
+
+                    <ForgotPassword open={open} handleClose={handleClose} />
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        onClick={validateInputs}
+                    >
+                        Sign in
+                    </Button>
+                </Box>
             </Card>
-        </Box>
+        </SignInContainer>
     );
 }
