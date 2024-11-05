841d539fbc4bdc1fbb906930e630b1080e73338c
C. Rambow <rambow.dev@elomagic.de>
2024-11-05 05:27:25 +0100

    Test fixed

--- src/auth/useAuth.ts
+++ src/auth/useAuth.ts
@@ -2,7 +2,7 @@ import {AuthContext, AuthContextProps} from "./Auth.tsx";
 import {useContext} from "react";
 
 export const useAuth = (): AuthContextProps => {
-    const context = useContext(AuthContext);
+    const context: AuthContextProps | undefined = useContext(AuthContext);
 
     if (!context) {
         console.warn("AuthProvider context is undefined, please verify you are calling useAuth() as child of a <AuthProvider> component.");
