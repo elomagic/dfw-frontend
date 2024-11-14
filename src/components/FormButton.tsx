f15ef643840f8c811e54d858fa6984d36571cd87
C. Rambow <rambow.dev@elomagic.de>
2024-11-14 12:34:33 +0100

    ...

--- src/components/FormButton.tsx
+++ src/components/FormButton.tsx
@@ -1,21 +1,23 @@
 import {MouseEventHandler} from "react";
 import {Button} from "@mui/material";
-import {Add} from "@mui/icons-material";
+import {Save} from "@mui/icons-material";
 import Grid from "@mui/material/Grid2";
+import * as React from "react";
 
 interface FormButtonSaveProps {
     label: string;
     onClick: MouseEventHandler | undefined;
+    startIcon?: React.ReactNode;
 }
 
-export default function FormButton({ label, onClick}: Readonly<FormButtonSaveProps>) {
+export default function FormButton({ label, onClick, startIcon}: Readonly<FormButtonSaveProps>) {
 
     return (
         <Grid size={12}>
             <Button variant="contained"
                     onClick={onClick}
                     size="small"
-                    startIcon={<Add />}>
+                    startIcon={startIcon ?? <Save />}>
                 {label}
             </Button>
         </Grid>
