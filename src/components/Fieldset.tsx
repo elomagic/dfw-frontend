b0b5799c9ed74971aa9ef0f7f57e4bf761ee0cba
Carsten Rambow <developer@elomagic.de>
2024-11-28 12:19:39 +0100

    l&f

--- src/components/Fieldset.tsx
+++ src/components/Fieldset.tsx
@@ -10,10 +10,17 @@ export const Fieldset = ({ label, children, ...props }: FieldsetProps) => {
     return (
         <Box component="fieldset" {...props}
              sx={{
-                 borderColor: "rgba(255, 255, 255, 0.7)",
+                 borderColor: "rgba(255, 255, 255, 0.3)",
                  borderRadius: 1,
                  borderWidth: 1,
-                 margin: 0
+                 margin: 0,
+                 "&:hover": {
+                     borderColor: "white"
+                 },
+                 "&:focus-within": {
+                     borderColor: "#90caf9",
+                     borderWidth: 2,
+                 }
              }}
         >
             {label && (<Typography
