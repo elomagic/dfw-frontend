012c03cf12cc54ab33f46a7aaedfc2677e7c9dd1
C. Rambow <rambow.dev@elomagic.de>
2024-12-03 20:01:43 +0100

    vite-plugin-node-polyfills removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {nodePolyfills} from "vite-plugin-node-polyfills";
 import zipPack from "vite-plugin-zip-pack";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    nodePolyfills(),
     react(),
     zipPack({
       outFileName: 'dfw-frontend.zip',
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,9 @@
 /// <reference types="vitest/config" />
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import {nodePolyfills} from "vite-plugin-node-polyfills";
 
 export default defineConfig({
     plugins: [
-        nodePolyfills(),
         react()
     ],
     test: {
