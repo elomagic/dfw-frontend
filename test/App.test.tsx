8f696da21043045ec289313691b4419789d4a9a4
Carsten Rambow <developer@elomagic.de>
2025-01-24 11:04:48 +0100

    ...

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,12 +1,17 @@
-import { render } from '@testing-library/react'
 import { expect, test } from 'vitest'
-import App from '../src/App';
+import { render } from '@testing-library/react'
+import {StrictMode} from "react";
+import App from "../src/App";
+import '../src/index.css'
+import '../src/i18n/config';
 import {AuthProvider} from "../src/auth/Auth";
 
 test('should render App', () => {
     expect(render(
-        <AuthProvider>
-            <App />
-        </AuthProvider>
+        <StrictMode>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </StrictMode>
     )).toBeTruthy();
 });
