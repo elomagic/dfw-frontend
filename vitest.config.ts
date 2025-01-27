09b0442cc346ff26a09191057871caf55cb592a1
Carsten Rambow <developer@elomagic.de>
2025-01-27 08:57:08 +0100

    ...

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
-import path from "path";
+import path from "node:path";
 /// <reference types="vitest/config" />
-import {defineConfig} from 'vite'
+import {defineConfig, configDefaults} from "vitest/config";
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -16,5 +16,22 @@ export default defineConfig({
     test: {
         environment: 'jsdom',
         setupFiles: ['./vitest.setup.ts'],
+        include: ["test/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+        exclude: [
+            ...configDefaults.exclude,
+            "**/src/component/element/**",
+            "**/src/style/**",
+            "**/src/**/*i18n.{js,jsx,ts,tsx}",
+        ],
+    },
+    environments: {
+        client: {
+            dev: {
+                /* 2025-01-27 Added by elo to suppress error message "to many files open".
+                 * https://github.com/vitest-dev/vitest/issues/7345
+                 */
+                preTransformRequests: false
+            }
+        }
     }
 })
